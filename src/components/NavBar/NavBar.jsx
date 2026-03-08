caf30e54b591fd12593fb6bccea9395485ec038f
Amishi Arora <amishia859@gmail.com>
2026-03-08 16:50:40 -0700

    Added favicon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import styles from "./NavBar.module.css"
-import logoImage from "../../assets/logoImage.png"
+import logo from "../../assets/logo.png"
 export default function NavBar() {
     return (
         <nav className={styles.navBar}>
             <div>
-                <img className = {styles.logoImage} src={logoImage} alt="logo image" />
+                <img className = {styles.logoImage} src={logo} alt="logo image" />
             </div>
             <div className={styles.links}>
                 <a href="#homeSection">Home</a>
