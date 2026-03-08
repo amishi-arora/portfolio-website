4bb6580e99143c839cd74d63a2fd0e0dc789e47b
Amishi Arora <amishia859@gmail.com>
2026-03-07 20:49:54 -0800

    Added description to about section

---
+++ src/components/AboutDescription/AboutDescription.jsx
@@ -0,0 +1,10 @@
+import styles from "./AboutDescription.module.css";
+export default function AboutDecription() {
+    return (
+        <div className={styles.textContainer}>
+            <p>Hi, I'm Amishi! <br /> I'm a third year Computer Science student at the University of British Columbia,
+                passionate about building creative solutions that make a real-world impact. <br /> I'm currently interning as a Software QA Engineer at the BCMEA. <br /> Feel free to connect with me on <a href="https://www.linkedin.com/in/amishi-arora/">LinkedIn</a>
+            </p>
+        </div>
+    )
+}
--- src/sections/AboutSection/AboutSection.jsx
+++ src/sections/AboutSection/AboutSection.jsx
@@ -1,8 +1,11 @@
 import AboutImage from "../../components/AboutImage/AboutImage.jsx";
+import AboutDecription from "../../components/AboutDescription/AboutDescription.jsx";
+import styles from "./AboutSection.module.css";
 export default function AboutSection() {
     return <section>
-        <div>
+        <div className = {styles.aboutContent}>
             <AboutImage></AboutImage>
+            <AboutDecription></AboutDecription>
         </div>
     </section>
 }
