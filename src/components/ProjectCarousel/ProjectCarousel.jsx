954bd551ff6ec314e5f75e845059149c803ad137
Amishi Arora <amishia859@gmail.com>
2026-03-16 20:26:03 -0700

    Added navigation dots to project carousel

--- src/components/ProjectCarousel/ProjectCarousel.jsx
+++ src/components/ProjectCarousel/ProjectCarousel.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react'
 import styles from "./ProjectCarousel.module.css"
 import ProjectSlide from '../ProjectSlide/ProjectSlide'
@@ -5,27 +6,51 @@ import projectDescriptions from "../../ProjectDescriptions.js";
 
 export default function ProjectCarousel() {
     const [carouselRef, carouselApi] = useEmblaCarousel({ loop: false, duration: 30 })
-    const goToPrev = () => carouselApi.scrollPrev() 
-    const goToNext = () => carouselApi.scrollNext()
+    const [scrollSnaps, setScrollSnaps] = useState([]);
+
+    const goToPrev = () => carouselApi?.scrollPrev()
+    const goToNext = () => carouselApi?.scrollNext()
+
+    const goTo = (index) => carouselApi?.scrollTo(index)
+    const setupSnaps = (carouselApi) => setScrollSnaps(carouselApi.scrollSnapList())
+
+    useEffect(() => {
+        if (!carouselApi) return;
+        setupSnaps(carouselApi)
+        carouselApi.on('reinit', setupSnaps)
+    }, [carouselApi])
+
 
     return (
         <div>
             <div className={styles.viewport} ref={carouselRef}>
                 <div className={styles.container}>
-                    {projectDescriptions.map(p => <ProjectSlide key = {p.title} title = {p.title} description = {p.description} image = {p.image} tags = {p.tags} gitHubLink = {p.gitHubLink} demoLink = {p.demoLink}/>)}
+                    {projectDescriptions.map(p => <ProjectSlide key={p.title} title={p.title} description={p.description} image={p.image} tags={p.tags} gitHubLink={p.gitHubLink} demoLink={p.demoLink} />)}
                 </div>
             </div>
 
-            <div>
-                
-            </div>
-            <button className = {styles.nextAndPrevButt} onClick={goToPrev}>
-                &#8249;
-            </button>
-            <button className = {styles.nextAndPrevButt}  onClick={goToNext}>
-                &#8250;
-            </button>
 
+            <div className={styles.carouselControls}>
+                <div >
+                    <button className={styles.nextAndPrevButt} onClick={goToPrev}>
+                        &#8249;
+                    </button>
+                    <button className={styles.nextAndPrevButt} onClick={goToNext}>
+                        &#8250;
+                    </button>
+                </div>
+
+                <div className={styles.dotsContainer}>
+                    {scrollSnaps.map((_, index) => (
+                        <button
+                            className={styles.slideDots}
+                            key={index}
+                            onClick={() => goTo(index)}
+                        >
+                        </button>
+                    ))}
+                </div>
+            </div>
 
         </div>
     )
