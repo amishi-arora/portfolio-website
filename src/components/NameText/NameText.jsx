34678452f39b8bdbadee63a7f1dbcadeab5d4f8f
Amishi Arora <amishia859@gmail.com>
2026-03-21 14:05:55 -0700

    Changed name animation

--- src/components/NameText/NameText.jsx
+++ src/components/NameText/NameText.jsx
@@ -1,14 +1,10 @@
-import {ReactTyped} from "react-typed"; 
-import styles from './NameText.module.css'; 
+import { ReactTyped } from "react-typed";
+import styles from './NameText.module.css';
 
 export default function NameText() {
     return (
         <h1 className={styles.nameText}>
-            <ReactTyped
-                strings={["Amishi Arora"]}
-                typeSpeed={125}
-                showCursor={false}
-            />
+            Amishi Arora
         </h1>
-    ); 
+    );
 }
