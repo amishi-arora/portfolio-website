ca944893a5757ca368e18472cc8fd40e0d93fdd9
Amishi Arora <amishia859@gmail.com>
2026-03-30 18:01:40 -0700

    Cleaned up state management for carousel

--- src/components/ProjectCarousel/ProjectCarousel.jsx
+++ src/components/ProjectCarousel/ProjectCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react'
 import styles from "./ProjectCarousel.module.css"
 import ProjectSlide from '../ProjectSlide/ProjectSlide'
@@ -7,49 +7,37 @@ import projectDescriptions from "../../ProjectDescriptions.js";
 export default function ProjectCarousel() {
     const [carouselRef, carouselApi] = useEmblaCarousel({ loop: false, duration: 30 })
     const [scrollSnaps, setScrollSnaps] = useState([]);
-    const [highlightedIndex, setIndex] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     const goToPrev = () => carouselApi?.scrollPrev()
     const goToNext = () => carouselApi?.scrollNext()
-
     const goTo = (index) => carouselApi?.scrollTo(index)
-    const setupSnaps = (carouselApi) => setScrollSnaps(carouselApi.scrollSnapList())
-    let nextButtonDisabled = false;
-    let prevButtonDisabled = false;
 
-    if (highlightedIndex === 0) {
-        prevButtonDisabled = true;
-    } else if (highlightedIndex === scrollSnaps.length - 1) {
-        nextButtonDisabled = true;
-    }
+    const setupSnaps = useCallback((api) => {
+        setScrollSnaps(api.scrollSnapList());
+    }, []);
+
+    const onSelect = useCallback((api) => {
+        setSelectedIndex(api.selectedScrollSnap());
+    }, []);
 
     useEffect(() => {
         if (!carouselApi) return;
         setupSnaps(carouselApi)
-        carouselApi.on('reinit', setupSnaps)
-    }, [carouselApi])
-
-    function handleDotClick(index) {
-        goTo(index);
-        setIndex(index);
-    }
-
-    function handleNextClick() {
-        setIndex(prevIndex => prevIndex + 1);
-        goToNext();
-    }
+        onSelect(carouselApi);
+        carouselApi.on('select', onSelect);
+        carouselApi.on('reinit', setupSnaps); 
+    }, [carouselApi, setupSnaps, onSelect])
 
 
-    function handlePrevClick() {
-        setIndex(prevIndex => prevIndex - 1);
-        goToPrev();
-    }
+    const nextButtonDisabled = selectedIndex === scrollSnaps.length - 1;
+    const prevButtonDisabled = selectedIndex === 0;
 
     return (
         <div data-aos="fade-up" data-aos-delay="200" className={styles.wrapper}>
 
             <div className={styles.viewportAndButtons}>
-                <button className={styles.nextAndPrevButt} onClick={handlePrevClick} disabled={prevButtonDisabled}>
+                <button className={styles.nextAndPrevButt} onClick={goToPrev} disabled={prevButtonDisabled}>
                     &#8249;
                 </button>
                 <div className={styles.viewport} ref={carouselRef}>
@@ -57,18 +45,18 @@ export default function ProjectCarousel() {
                         {projectDescriptions.map(p => <ProjectSlide key={p.title} title={p.title} description={p.description} image={p.image} tags={p.tags} gitHubLink={p.gitHubLink} demoLink={p.demoLink} />)}
                     </div>
                 </div>
-                <button className={styles.nextAndPrevButt} onClick={handleNextClick} disabled={nextButtonDisabled}>
+                <button className={styles.nextAndPrevButt} onClick={goToNext} disabled={nextButtonDisabled}>
                     &#8250;
                 </button>
             </div>
             <div className={styles.dotsContainer}>
                 {
                     scrollSnaps.map((_, index) => {
-                        const isActive = index === highlightedIndex;
+                        const isActive = index === selectedIndex;
                         return <button
                             className={`${styles.slideDots} ${isActive ? styles.highlightedDot : ""}`}
                             key={index}
-                            onClick={() => handleDotClick(index)}
+                            onClick={() => goTo(index)}
                         >
                         </button>
                     })}
