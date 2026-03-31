7092c0276740b89b3a70b89d4c646a72e6aeff07
Amishi Arora <amishia859@gmail.com>
2026-03-30 17:17:09 -0700

    Added state to highlight navigation dots and disable next and prev buttons for project carousel

--- src/components/ProjectCarousel/ProjectCarousel.jsx
+++ src/components/ProjectCarousel/ProjectCarousel.jsx
@@ -7,12 +7,21 @@ import projectDescriptions from "../../ProjectDescriptions.js";
 export default function ProjectCarousel() {
     const [carouselRef, carouselApi] = useEmblaCarousel({ loop: false, duration: 30 })
     const [scrollSnaps, setScrollSnaps] = useState([]);
+    const [highlightedIndex, setIndex] = useState(0);
 
     const goToPrev = () => carouselApi?.scrollPrev()
     const goToNext = () => carouselApi?.scrollNext()
 
     const goTo = (index) => carouselApi?.scrollTo(index)
     const setupSnaps = (carouselApi) => setScrollSnaps(carouselApi.scrollSnapList())
+    let nextButtonDisabled = false;
+    let prevButtonDisabled = false;
+
+    if (highlightedIndex === 0) {
+        prevButtonDisabled = true;
+    } else if (highlightedIndex === scrollSnaps.length - 1) {
+        nextButtonDisabled = true;
+    }
 
     useEffect(() => {
         if (!carouselApi) return;
@@ -20,12 +29,27 @@ export default function ProjectCarousel() {
         carouselApi.on('reinit', setupSnaps)
     }, [carouselApi])
 
+    function handleDotClick(index) {
+        goTo(index);
+        setIndex(index);
+    }
+
+    function handleNextClick() {
+        setIndex(prevIndex => prevIndex + 1);
+        goToNext();
+    }
+
+
+    function handlePrevClick() {
+        setIndex(prevIndex => prevIndex - 1);
+        goToPrev();
+    }
 
     return (
         <div data-aos="fade-up" data-aos-delay="200" className={styles.wrapper}>
 
             <div className={styles.viewportAndButtons}>
-                <button className={styles.nextAndPrevButt} onClick={goToPrev}>
+                <button className={styles.nextAndPrevButt} onClick={handlePrevClick} disabled={prevButtonDisabled}>
                     &#8249;
                 </button>
                 <div className={styles.viewport} ref={carouselRef}>
@@ -33,19 +57,21 @@ export default function ProjectCarousel() {
                         {projectDescriptions.map(p => <ProjectSlide key={p.title} title={p.title} description={p.description} image={p.image} tags={p.tags} gitHubLink={p.gitHubLink} demoLink={p.demoLink} />)}
                     </div>
                 </div>
-                <button className={styles.nextAndPrevButt} onClick={goToNext}>
+                <button className={styles.nextAndPrevButt} onClick={handleNextClick} disabled={nextButtonDisabled}>
                     &#8250;
                 </button>
             </div>
             <div className={styles.dotsContainer}>
-                {scrollSnaps.map((_, index) => (
-                    <button
-                        className={styles.slideDots}
-                        key={index}
-                        onClick={() => goTo(index)}
-                    >
-                    </button>
-                ))}
+                {
+                    scrollSnaps.map((_, index) => {
+                        const isActive = index === highlightedIndex;
+                        return <button
+                            className={`${styles.slideDots} ${isActive ? styles.highlightedDot : ""}`}
+                            key={index}
+                            onClick={() => handleDotClick(index)}
+                        >
+                        </button>
+                    })}
             </div>
         </div>
     )
