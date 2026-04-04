7ff623bb42d80543fcbfb12dc354f17317a1d226
Amishi Arora <amishia859@gmail.com>
2026-04-04 15:23:50 -0700

    Added contact icons on navigation bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import styles from "./NavBar.module.css"
+
 export default function NavBar() {
     return (
         <nav className={styles.navBar}>
@@ -6,6 +7,11 @@ export default function NavBar() {
                 <a href="#homeSection">Home</a>
                 <a href="#aboutSection">About</a>
                 <a href="#projectSection">Projects</a>
+            </div>
+            <div className={styles.contact}>
+                <a href="https://github.com/amishi-arora" target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/amishi-arora/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="mailto:amishia859@gmail.com" target="_blank"><i class="fa-regular fa-envelope"></i></a>
             </div>
         </nav>
     )
